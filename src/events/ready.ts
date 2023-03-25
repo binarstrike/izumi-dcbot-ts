fd87a34a7a1fc6e0f4e91e3d01e1ea8e64083b0f
Binar Nugroho <92077948+binarstrike@users.noreply.github.com>
2023-03-25 10:34:38 +0700

    fix: registering slash command

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,10 +1,13 @@
 import { Event } from "../structures/Event"
-import { ActivityType } from "discord.js"
+import { client } from ".."
 
-export default new Event("ready", (i) => {
-  i.user.setPresence({
+export default new Event("ready", async function (client_) {
+  console.log("Registering slash command...")
+  client.application.commands.cache.clear()
+  await client.application.commands.set(client.slashCommands)
+  client_.user.setPresence({
+    activities: [{ name: "Genshin Impact 24/7", type: 0 }],
     status: "dnd",
-    activities: [{ name: "Test BOT", type: ActivityType.Playing }],
   })
-  console.log(`Bot is online ${i.user.tag}`)
+  console.log(`Bot is online ${client_.user.tag}`)
 })
