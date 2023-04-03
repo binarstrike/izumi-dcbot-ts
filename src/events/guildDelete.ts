f99db2ca80d9d93775f3c1d7c82a64d44f78010b
Binar Nugroho <92077948+binarstrike@users.noreply.github.com>
2023-04-03 11:29:28 +0700

    fix: remove clear slash command function

--- src/events/guildDelete.ts
+++ src/events/guildDelete.ts
@@ -6,7 +6,6 @@ export default new Event("guildDelete", async function (guild) {
   try {
     await prisma.channelConfiguration.delete({ where: { idServer: guild.id } })
     await prisma.serverData.delete({ where: { serverId: guild.id } })
-    await clearCommandGuild(guild)
     console.log(`Kicked from Server: ${guild.name} - ${guild.id}`)
   } catch (error) {
     console.log(error.message)
