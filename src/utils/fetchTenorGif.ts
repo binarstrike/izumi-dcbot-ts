1878d7b4af66556b83ff3ded420d8f921c8ceb2a
Binar Nugroho <92077948+binarstrike@users.noreply.github.com>
2023-04-07 13:03:04 +0700

    add type for fetching results

--- src/utils/fetchTenorGif.ts
+++ src/utils/fetchTenorGif.ts
@@ -1,25 +1,46 @@
 import axios from "axios"
 
-const SEARCH_ENDPOINT: string = `https://tenor.googleapis.com/v2/search?key=${process.env.TENOR_API_KEY}&random=true&media_filter=gif`
+const SEARCH_ENDPOINT = `https://tenor.googleapis.com/v2/search?key=${process.env.TENOR_API_KEY}&random=true&media_filter=gif`
 
-type fetchParam = {
+type TFetchGIFParam = {
   q: string
   limit?: number
   client_key?: string
 }
 
-const genTenorAPIUrl = (params: fetchParam): string =>
+type TFetchGIFResult = {
+  id: string
+  title: string
+  media_formats: {
+    gif: {
+      url: string
+      duration: number
+      preview: string
+      dims: number[]
+      size: number
+    }
+  }
+  created: number
+  content_description: string
+  itemurl: string
+  url: string
+  tags: string[]
+  flags: string[] | number[]
+  hasaudio: boolean
+}
+
+const genTenorAPIUrl = (params: TFetchGIFParam): string =>
   `${SEARCH_ENDPOINT}&limit=${params.limit || 20}&client_key=${
     params.client_key || "any_key"
   }&q=${params.q.split(" ").join("+")}`
 
-export async function fetchGif(params: fetchParam): Promise<string[]> {
+export async function fetchGif(params: TFetchGIFParam): Promise<string[]> {
   try {
     const urls: string[] = []
     const fetchData = await axios(genTenorAPIUrl(params), { method: "GET" })
     if (typeof fetchData.data?.results === "object") {
       fetchData.data.results
-        .map((result: any) => result.media_formats.gif.url)
+        .map((result: TFetchGIFResult) => result.media_formats.gif.url)
         .forEach((url: string) => urls.push(url))
       return urls
     }
