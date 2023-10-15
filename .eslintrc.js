003e59f0217e864e1766aef0dfc6af0b1656fe36
Binar Nugroho <binarnugroho775@gmail.com>
2023-10-15 09:20:09 +0700

    lint: ignore generated client folder from linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,7 @@ module.exports = {
     node: true,
     // jest: true,
   },
-  ignorePatterns: [".eslintrc.js", "dist"],
+  ignorePatterns: [".eslintrc.js", "dist", "src/generated"],
   rules: {
     "@typescript-eslint/interface-name-prefix": "off",
     "@typescript-eslint/explicit-function-return-type": "off",
